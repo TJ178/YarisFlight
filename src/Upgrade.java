3f8d2444e6ab00341a559f2aed91cbc302960212
Michael LaBarbera <michael.jlabarbera@gmail.com>
2020-01-09 09:27:11 -0800

    Added some functionality to WingsUpgrade and UpgradeClass, added YarisGlider.png

--- src/Upgrade.java
+++ src/Upgrade.java
@@ -4,10 +4,44 @@ import java.awt.Image;
 public class Upgrade {
 	private String imgPath;
 	private Image img;
-	private int weight;
-	private int drag;
+	protected int weight;
+	protected int drag;
+	protected int lift;
+	protected int speed;
 	
 	public Upgrade() {
 		
+	}
+	
+	public String getImgPath() {
+		return imgPath;
+	}
+
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+
+	public Image getImg() {
+		return img;
+	}
+
+	public void setImg(Image img) {
+		this.img = img;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getDrag() {
+		return drag;
+	}
+
+	public void setDrag(int drag) {
+		this.drag = drag;
 	}
 }
