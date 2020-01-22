9d5857f80fd6a5d640649544c6ad674eb7fec87a
Timothy Jacques <tjacques888@gmail.com>
2020-01-22 09:06:50 -0800

    scorekeeping basis

--- src/Score.java
+++ src/Score.java
@@ -3,9 +3,43 @@ public class Score {
 	private int altitudeMax;
 	private int distanceMax;
 	private int speedMax;
+	private long startTime;
 	
-	private int newMoney;
+	public Score() {
+		altitudeMax = 0;
+		distanceMax = 0;
+		speedMax = 0;
+		startTime = System.currentTimeMillis();
+	}
 	
+	public void updateScores(int alt, int dist, int speed) {
+		if(alt > altitudeMax) {
+			altitudeMax = alt;
+		}
+		
+		if(dist > distanceMax) {
+			distanceMax = dist;
+		}
+		
+		if(speed > speedMax) {
+			speedMax = speed;
+		}
+	}
 	
-	public Score(int alt, int dist, int speed)
+	public int getMaxAlt() {
+		return altitudeMax;
+	}
+	
+	public int getMaxDist() {
+		return distanceMax;
+	}
+	
+	public int getMaxSpeed() {
+		return speedMax;
+	}
+	
+	public int getTimeElapsed() {
+		return (int)(startTime - System.currentTimeMillis());
+	}
+	
 }
